bcd7b02f7168bc57da7fa1a34caf19171e512154
Flex <flex@Flexs-MacBook-Air.local>
2023-01-08 22:33:02 -0800

    test

--- streamtest_2.py
+++ streamtest_2.py
@@ -19,7 +19,7 @@ import complexities
 st.image("https://www.physiotherapyjobscanada.ca/files/pictures/Full_Logo_Black.png")
 
 st.markdown('''<span style="color: limegreen; font-weight: bold; font-size: 2.5em"> 
-        Welcome to ConcussionRx!Will this change?
+        Welcome to ConcussionRx!Let's change this again on push
         </span>''', 
         unsafe_allow_html=True
 )
